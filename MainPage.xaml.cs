768eaa006037a482dab16566a0eb20b0e19e26e0
Rose Anne Gito <69665835+Rosie04102003@users.noreply.github.com>
2023-10-09 21:46:22 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace MAUIApp3;
+
+public partial class MainPage : ContentPage
+{
+    string phoneNumHolder = "";
+    bool isNumeric;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	public async void OnDial(object sender, EventArgs e)
+	{
+        phoneNumHolder = PhoneNum.Text;
+        isNumeric = long.TryParse(phoneNumHolder, out _);
+
+
+        if (isNumeric == false)
+		{
+            await DisplayAlert("Invalid Input", "Please enter a valid phone number", "OK");
+
+        } else
+        {
+            try
+            {
+                PhoneDialer.Default.Open(phoneNumHolder);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert(ex.Message, "Please enter a valid phone number", "OK");
+            }
+        }
+	}
+}
+
+
